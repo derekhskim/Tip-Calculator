7a503dbb20a69aae4047e928786d9b397a7a549e
[treasure3210] <treasure3210@gmail.com>
2023-04-23 14:11:21 -0600

    Initial Commit

---
+++ tip-calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tip-calculator
+//
+//  Created by Derek Kim on 2023-04-23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
