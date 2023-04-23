04a3a0bf4e2709592aad373a6fa452c14e397e0a
[treasure3210] <treasure3210@gmail.com>
2023-04-23 14:39:57 -0600

    Add resources, Setup SceneDelegate, Setup Views to reuse, and setup layout of the VC with snap kit

---
+++ tip-calculator/View/ResultView.swift
@@ -0,0 +1,25 @@
+//
+//  ResultView.swift
+//  tip-calculator
+//
+//  Created by Derek Kim on 2023-04-23.
+//
+
+import UIKit
+
+class ResultView: UIView {
+    
+    init() {
+        super.init(frame: .zero)
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func layout() {
+        backgroundColor = .gray
+    }
+    
+}
---
+++ tip-calculator/View/TipInputView.swift
@@ -0,0 +1,25 @@
+//
+//  TipInputView.swift
+//  tip-calculator
+//
+//  Created by Derek Kim on 2023-04-23.
+//
+
+import UIKit
+
+class TipInputView: UIView {
+    
+    init() {
+        super.init(frame: .zero)
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func layout() {
+        backgroundColor = .systemPink
+    }
+    
+}
