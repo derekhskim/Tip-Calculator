dd766ac891f38266ded974741413d14c862ac974
[treasure3210] <treasure3210@gmail.com>
2023-04-23 18:22:41 -0600

    Configure labels and constraints of ResultView and shadowView for common usage

---
+++ tip-calculator/Helper/UIView+.swift
@@ -0,0 +1,24 @@
+//
+//  UIView+.swift
+//  tip-calculator
+//
+//  Created by Derek Kim on 2023-04-23.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func addShadow(offset: CGSize, color: UIColor, radius: CGFloat, opacity: Float) {
+        layer.cornerRadius = radius
+        layer.masksToBounds = false
+        layer.shadowOffset = offset
+        layer.shadowColor = color.cgColor
+        layer.shadowRadius = radius
+        layer.shadowOpacity = opacity
+        let backgroundCGColor = backgroundColor?.cgColor
+        backgroundColor = nil
+        layer.backgroundColor = backgroundCGColor
+    }
+    
+}
