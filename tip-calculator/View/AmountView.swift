1b5ab1067b75a2cfda2024064ecaf7d3e8d6d2ab
[treasure3210] <treasure3210@gmail.com>
2023-04-23 18:35:42 -0600

    Configure AmountView and give file of it's own, and set constraints with it's own initializers

---
+++ tip-calculator/View/AmountView.swift
@@ -0,0 +1,66 @@
+//
+//  AmountView.swift
+//  tip-calculator
+//
+//  Created by Derek Kim on 2023-04-23.
+//
+
+import UIKit
+
+class AmountView: UIView {
+    
+    private let title: String
+    private let textAlignment: NSTextAlignment
+    
+    private lazy var titleLabel: UILabel = {
+        LabelFactory.build(
+            text: title,
+            font: ThemeFont.regular(ofSize: 18),
+            textColor: ThemeColor.text,
+            textAlignment: textAlignment)
+    }()
+    
+    private lazy var amountLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = textAlignment
+        label.textColor = ThemeColor.primary
+        let text = NSMutableAttributedString(
+            string: "$0",
+            attributes: [
+                .font: ThemeFont.bold(ofSize: 24)
+            ])
+        text.addAttributes([
+            .font: ThemeFont.bold(ofSize: 16)
+        ], range: NSMakeRange(0, 1))
+        label.attributedText = text
+        return label
+    }()
+    
+    private lazy var vStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [
+            titleLabel,
+            amountLabel
+        ])
+        stackView.axis = .vertical
+        return stackView
+    }()
+    
+    init(title: String, textAlignment: NSTextAlignment) {
+        self.title = title
+        self.textAlignment = textAlignment
+        super.init(frame: .zero)
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func layout() {
+        addSubview(vStackView)
+        vStackView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+    
+}
--- tip-calculator/View/ResultView.swift
+++ tip-calculator/View/ResultView.swift
@@ -51,9 +51,13 @@ class ResultView: UIView {
     
     private lazy var hStackView: UIStackView = {
         let stackView = UIStackView(arrangedSubviews: [
-            AmountView(),
+            AmountView(
+                title: "Total bill",
+                textAlignment: .left),
             UIView(),
-            AmountView()
+            AmountView(
+                title: "Total tip",
+                textAlignment: .right)
         ])
         stackView.axis = .horizontal
         stackView.distribution = .fillEqually
@@ -94,23 +98,6 @@ class ResultView: UIView {
         let view = UIView()
         view.heightAnchor.constraint(equalToConstant: height).isActive = true
         return view
-    }
-    
-}
-
-class AmountView: UIView {
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        layout()
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    private func layout() {
-        backgroundColor = .red
     }
     
 }
